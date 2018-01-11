9591fc54cae1980166c653fc55ba8838bc56d1a4
Bartosz Górka <bartosz.gorka@appunite.com>
2018-01-11 21:44:59 +0100

    Login fxml

--- src/main/java/put/sk/publish/Client.java
+++
@@ -1,9 +0,0 @@
-package put.sk.publish;
-
-public class Client {
-
-    public static void main(String args[]) {
-        Connection connection = new Connection("localhost", 80);
-        System.out.println(connection.toString());
-    }
-}
