d78309b5d982eb4f549bb3970fbac88310bb2a13
Bartosz Górka <bartosz.gorka@appunite.com>
2018-01-12 20:16:44 +0100

    Main view form

--- src/main/java/put/sk/publish/MainView.java
+++ src/main/java/put/sk/publish/MainView.java
@@ -1,7 +1,10 @@
 package put.sk.publish;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * Main view
@@ -19,5 +22,29 @@ public class MainView {
      * Selected article text area
      */
     public TextArea areaArticleText;
+    /**
+     * Group with selected article
+     */
+    public AnchorPane groupSelectedArticle;
+    /**
+     * Article title - used in create new article
+     */
+    public TextField inputArticleTitle;
+    /**
+     * Button read article
+     */
+    public Button buttonRead;
+    /**
+     * Button write new article
+     */
+    public Button buttonWrite;
+    /**
+     * Button clear text
+     */
+    public Button buttonClear;
+    /**
+     * Button send new article
+     */
+    public Button buttonSend;
 
 }
