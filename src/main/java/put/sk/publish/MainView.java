4a6848e204148d507aa7bdfe9cc4b4bb64b0aec8
Bartosz Górka <bartosz.gorka@appunite.com>
2018-01-12 17:02:19 +0100

    Basic main view draft

---
+++ src/main/java/put/sk/publish/MainView.java
@@ -0,0 +1,23 @@
+package put.sk.publish;
+
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+
+/**
+ * Main view
+ */
+public class MainView {
+    /**
+     * List view with queues
+     */
+    public ListView listQueue;
+    /**
+     * List with articles in selected queue
+     */
+    public ListView listArticle;
+    /**
+     * Selected article text area
+     */
+    public TextArea areaArticleText;
+
+}
